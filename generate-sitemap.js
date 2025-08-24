e8cb4c694706be0b0d6db605e7f2ee48fb48e369
stefan.balog <stefan.balog1993@gmail.com>
2025-08-24 12:40:19 +0200

    generate sitemap

---
+++ generate-sitemap.js
@@ -0,0 +1,36 @@
+import fs from "fs";
+import { url } from "inspector";
+import { SitemapStream, streamToPromise } from "sitemap";
+import { createGzip } from "zlib";
+
+(async () => {
+  const sitemap = new SitemapStream({
+    hostname: "https://fitfusiondemo.netlify.app/",
+  });
+  const writeStream = fs.createWriteStream("./public/sitemap.xml.gz");
+  sitemap.pipe(createGzip()).pipe(writeStream);
+
+  const routes = [
+    "/",
+    "/welcome/login",
+    "/welcome/signup",
+    "/welcome/forgot-password",
+    "/confirm-email",
+    "/password-reset",
+    "/dashboard/home",
+    "/dashboard/update-user",
+    "/dashboard/workouts",
+    "/dashboard/workouts/123",
+    "/dashboard/workouts/123/45",
+    "/dashboard/workout-calendar",
+  ];
+
+  routes.forEach((route) => {
+    sitemap.write({ url: route, changefreq: "weekly", priority: 0.8 });
+  });
+
+  sitemap.end();
+
+  await streamToPromise(sitemap);
+  console.log("Sitemap generated successfully.");
+})();
