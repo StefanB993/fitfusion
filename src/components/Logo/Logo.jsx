3541ba472fc5affedd2ac787b2851c4b053d36a2
stefan.balog <stefan.balog1993@gmail.com>
2025-09-07 22:17:24 +0200

    change logo url

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ function Logo({ size = 10 }) {
   return (
     <img
       style={style}
-      src="https://fdrjcrrrxwntvbtcheks.supabase.co/storage/v1/object/sign/logo/logo.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV85YjA5NzczNi1mYmUyLTQ3MDctYWJiMS1hZTFlNWM4ZmNhODEiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJsb2dvL2xvZ28ucG5nIiwiaWF0IjoxNzUwMDcwNzQyLCJleHAiOjE5MDc3NTA3NDJ9.V2uF0onAqpcqdXz-Taexfos573mSqDHhZMLexW_jm1s"
+      src="https://fdrjcrrrxwntvbtcheks.supabase.co/storage/v1/object/sign/logo/logo.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV82NWFlZmE1Yy05MGQzLTQ1MDAtODlkZC1hOWFmYzcwYzZkZTYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJsb2dvL2xvZ28ucG5nIiwiaWF0IjoxNzU3Mjc2MTMzLCJleHAiOjE3ODg4MTIxMzN9.Zrsh5KPGwkUMg1oiyRMHIP44GEGYrPRlEby16E0U-yc"
       alt=""
     />
   );
